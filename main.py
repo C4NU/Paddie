a376b6ebf17add3f4e7f8a9a72830cc5f94632ff
CANU <canu1832@gmail.com>
2023-08-11 00:39:35 +0900

    [ADD]
    main.py: PyQT 테스트 스크립트

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
-print("Test")
+import sys	# Module
+from PyQt5.QtWidgets import QApplication, QWidget	# PyQt5 GUI Module
+from PIL import Image	# WebP Converter Module
+
+
+class MyApp(QWidget):
+	def __init__(self):
+		super().__init__()
+		self.initUI()
+        
+	def initUI(self):
+		self.setWindowTitle('My First Application')
+		self.move(300, 300)
+		self.resize(400, 200)
+		self.show()
+
+
+if __name__ == '__main__':
+   app = QApplication(sys.argv)
+   ex = MyApp()
+   sys.exit(app.exec_())
