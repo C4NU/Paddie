8c2c09352f9de317c6da5b8d5989c4aa44e9c2c7
CANU <canu1832@gmail.com>
2023-08-14 13:14:49 +0900

    [ADD] 프로그램 실행 순서 작성

--- WebP_module.py
+++ WebP_module.py
@@ -1,12 +1,17 @@
 from PIL import Image
 
 class Converter():
+	#GetImage -> ConvertImage -> SaveImage로 순서
 	def __init__(self) -> None:
 		pass
 
 	def GetImage():
+		#main에서 drag drop으로 파일 읽어오면 webpmodule로 GetImage 호출해서 경로 받고 변환, 저장은 
 		pass
 
 	def ConvertImage():
 		pass
 	
+	def SaveImage():
+		
+		pass
