7ea5f2643e166b013ec67965b984e216e5fbd018
CANU <canu1832@gmail.com>
2023-08-16 01:29:25 +0900

    [ADD], [FIX] 썸네일 추가, 코드 최적화
    [ADD]
    main.py
    1. 파일 드래그 드롭 추가시 이미지 썸네일 표시되게 추가함.
    
    [FIX]
    main.py
    1. 난잡한 이전 주석 및 코드에 대하여 가지치기함

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
-from PyQt5.QtGui import *
+from PyQt5 import QtGui, QtWidgets, QtCore
 from PyQt5 import uic
 
 import sys	# 시스템 모듈
@@ -15,20 +15,7 @@ class WindowClass(QMainWindow, formClass) :
 		super().__init__()
 		self.converter = webp.Converter()
 		self.setupUi(self)
-		self.listWidget.itemClicked.connect(self.chkItemClicked)
 		self.buttonBox.clicked.connect(self.okButtonClicked)
-
-	def chkItemClicked(self):
-		print(self.listWidget.currentItem().text())
-
-	def addListWidget(self):
-		self.addItemText = self.line_addItem.text()
-		self.listWidget.addItem(self.addItemText)
-
-	def insertListWidget(self) :
-		self.insertRow = self.spin_insertRow.value()
-		self.insertText = self.line_insertItem.text()
-		self.listWidget.insertItem(self.insertRow, self.insertText)
 
 	def dragEnterEvent(self, event):
 		if event.mimeData().hasUrls():
@@ -44,71 +31,30 @@ class WindowClass(QMainWindow, formClass) :
 			self.links = []
 			# 드래그 드롭시 파일 추가되는 코드
 			for url in event.mimeData().urls():
-				if url.isLocalFile():
-					self.links.append(str(url.toLocalFile()))
-				else:
-					self.links.append(str(url.toString()))
-			self.listWidget.addItems(self.links)
+				icon = QtGui.QIcon(url.toLocalFile())
+				item = QtWidgets.QListWidgetItem(icon, url.toLocalFile())
+				
+				size = QtCore.QSize()
+				size.setHeight(128)
+				size.setWidth(128)
+				
+				item.setSizeHint(size)
+				
+				self.listWidget.addItem(item)
+
 		else:
 			event.ignore()
 
 	def okButtonClicked(self):
+		# 변환 실행 버튼 callback 함수
 		for index in range(self.listWidget.count()):
 			print(self.listWidget.item(index).text())
 			self.converter.ConvertImage(self.listWidget.item(index).text())
 		
 		self.listWidget.clear()
-	'''
-	def dropEvent(self, event):
-		files = [u.toLocalFile() for u in event.mimeData().urls()]
-		for f in files:
-			self.converter.ConvertImage(f)
-	'''
 
 if __name__ == "__main__" :
-	#QApplication : 프로그램을 실행시켜주는 클래스
 	app = QApplication(sys.argv) 
-
-	#WindowClass의 인스턴스 생성
 	myWindow = WindowClass() 
-
-	#프로그램 화면을 보여주는 코드
 	myWindow.show()
-
-	#프로그램을 이벤트루프로 진입시키는(프로그램을 작동시키는) 코드
 	app.exec_()
-'''
-class MainWidget(QMainWindow):
-	def __init__(self):
-		super().__init__()
-		self.converter = webp.Converter()
-		self.initUI()
-
-	def initUI(self):
-		self.setWindowTitle("WebP Converter")
-		self.setWindowIcon(QIcon('Resources/Icon@64X64_02.ico'))
-		self.resize(720, 480)
-		self.setAcceptDrops(True)
-
-	def dragEnterEvent(self, event):
-		if event.mimeData().hasUrls():
-			event.accept()
-		else:
-			event.ignore()
-
-	def dropEvent(self, event):
-		files = [u.toLocalFile() for u in event.mimeData().urls()]
-		for f in files:
-			self.converter.ConvertImage(f)
-
-
-
-if __name__ == '__main__':
-	app = QApplication(sys.argv)
-	path = os.path.join(os.path.dirname(sys.modules[__name__].__file__), 'Resources/Icon@64X64_01.ico')
-	app.setWindowIcon(QIcon(path))
-
-	ui = MainWidget()
-	ui.show()
-	sys.exit(app.exec_())
-'''
