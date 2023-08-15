dc771813ef8ebd6177e8478499c049b3f8b1325c
C4NU <canu1832@gmail.com>
2023-08-15 18:46:08 +0900

    [ADD] Converter Class 관련 수정 [FIX] 아이콘 폴더 경로 변경

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from PyQt5.QtWidgets import QMainWindow, QApplication	# PyQt5 모듈
 from PyQt5.QtGui import QIcon	# PyQt5 icon 모듈
 
 import sys	# 시스템 모듈
+import os
 
 import WebP_module as webp	# WebP 변환 모듈
 
@@ -9,11 +10,12 @@ import WebP_module as webp	# WebP 변환 모듈
 class MainWidget(QMainWindow):
 	def __init__(self):
 		super().__init__()
+		self.converter = webp.Converter()
 		self.initUI()
 
 	def initUI(self):
 		self.setWindowTitle("WebP Converter")
-		self.setWindowIcon(QIcon('Icon.ico'))
+		self.setWindowIcon(QIcon('Resources/Icon@64X64_02.ico'))
 		self.resize(720, 480)
 		self.setAcceptDrops(True)
 
@@ -26,12 +28,15 @@ class MainWidget(QMainWindow):
 	def dropEvent(self, event):
 		files = [u.toLocalFile() for u in event.mimeData().urls()]
 		for f in files:
-			print(f)
-			webp.Converter.ConvertImage(f)
+			self.converter.ConvertImage(f)
+
 
 
 if __name__ == '__main__':
 	app = QApplication(sys.argv)
+	path = os.path.join(os.path.dirname(sys.modules[__name__].__file__), 'Resources/Icon@64X64_01.ico')
+	app.setWindowIcon(QIcon(path))
+
 	ui = MainWidget()
 	ui.show()
 	sys.exit(app.exec_())
