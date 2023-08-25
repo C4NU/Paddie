275fa7066d2b686f76cca439e9a5cee2c3b1953a
CANU <canu1832@gmail.com>
2023-08-26 00:38:03 +0900

    [ADD] 생성자 변수 작성

--- Watermark_module.py
+++ Watermark_module.py
@@ -1,6 +1,14 @@
 from PIL import Image, ImageFont, ImageDraw
 
 class Watermark():
+	def __init__(self):
+		# 변수 초기화
+
+		# 폰트 사이즈 (초기)
+		self.fontSize = 30
+		# 폰트 (초기)
+		self.font = ImageFont.truetype("arial.ttf", self.fontSize)
+
 	def InsertWatermark(self, image, watermarkText):
 			width, height = image.size
 
@@ -16,6 +24,7 @@ class Watermark():
 
 			fontSize = int(fontSize/6)
 			font = ImageFont.truetype("arial.ttf", fontSize)
+
 			draw.text(xy=(width / 2 - (fontSize * 2), height / 2), text = watermarkText,font=font, fill=(0,0,0))
 			draw.text(xy=(width / 2 + (fontSize * 2), height / 2), text = watermarkText, font = font, fill = (255,255,255))
 
