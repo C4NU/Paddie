7b2ec6b9c79e883b9b7eb9f69cda38ed205d0573
CANU <canu1832@gmail.com>
2025-03-09 21:32:26 +0900

    [ADD] 스테가노그라피 모듈 테스트중

--- watermark_module.py
+++ watermark_module.py
@@ -6,12 +6,18 @@ class Watermark():
 	def __init__(self):
 		# 변수 초기화
 
-		# 폰트 사이즈 (초기
+		# 폰트 사이즈 (초기)
 		self.fontSize = 30
 		# 폰트 (초기)
-		self.font = ImageFont.truetype("Barlow-Light.ttf", self.fontSize)
+		if platform.system() == "Windows":
+			self.font = ImageFont.truetype(os.path.join(os.getcwd(), 'resources/Barlow-Light.ttf'), self.font_size)
+		else:
+			try:
+				self.font = ImageFont.truetype(os.path.join(os.path.dirname(sys.executable), "resources/Barlow-Light.ttf"), self.font_size)
+			except:
+				self.font = ImageFont.truetype(os.path.join(os.getcwd(), 'resources/Barlow-Light.ttf'), self.font_size)
 
-	def InsertWatermark(self, image, fontColor, watermarkText):
+	def insert_watermark(self, image, fontColor, watermarkText):
 			width, height = image.size
 
 			draw = ImageDraw.Draw(image)
@@ -26,7 +32,7 @@ class Watermark():
 
 			return image
 
-	def SetFontSize(self, x, y):
+	def set_font_size(self, x, y):
 		if x > y:
 			self.fontSize = y
 		elif y > x:
@@ -36,3 +42,17 @@ class Watermark():
 
 		self.fontSize = int(self.fontSize/6)
 
+	def steganography(self, image, watermarkText):
+		'''
+		이미지 파일에 저작권 요소를 삽입하는 함수
+		사용자 이름 / 이메일 등등 데이터 삽입
+		'''
+		
+		pass
+
+if __name__ == "__main__":
+	test_image = Image.open("resources/test/sample.jpg")
+	watermark_test = Watermark()
+
+	# 워터마크 삽입 기능 테스트
+	# 스테가노그래피 기능 테스트
